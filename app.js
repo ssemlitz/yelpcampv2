07348e75a1911e0ef67ab74baa78ebd8d1bb148e
ssemlitz <sdsemlitz@yahoo.com>
2023-01-24 13:43:37 -0500

    initialize express session

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utilities/ExpressError')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+mongoose.connect('mongodb://localhost:27017/yelpcamp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnitfiedTopology: true,
+  useFindAndModify: false
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
@@ -24,6 +30,19 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+  secret: 'thisshouldbeabettersecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
