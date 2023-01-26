8f9ff0f148cf607aa3681f4a5a4d91bf7170fb5d
ssemlitz <sdsemlitz@yahoo.com>
2023-01-26 12:23:04 -0500

    edit logout route with callback function

--- routes/users.js
+++ routes/users.js
@@ -29,24 +29,18 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
+    req.flash('success', 'Welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
 
-// router.get('/logout', (req, res) => {
-//     req.logout();
-//     req.flash('success', "Goodbye!");
-//     res.redirect('/campgrounds');
-// })
-
-// router.get('/logout', (req, res) => {
-//     req.logout(function (err) {
-//     if (err) { return next(err); }
-//     req.flash('success', "Goodbye!");
-//     res.redirect('/campgrounds');
-//     });
-// })
+router.get('/logout', (req, res) => {       
+    req.logout(function (err) {
+        // if (err) { return next(err); }
+        req.flash('success', "Goodbye!");
+        res.redirect('/campgrounds');
+    });
+})
 
 module.exports = router;
