4279d38906d392c1d33b50e8a8f5a57dd66dd769
ssemlitz <sdsemlitz@yahoo.com>
2023-01-31 11:13:57 -0500

    add default image to show and index page for campgrounds without any images

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,17 +1,16 @@
-mapboxgl.accessToken = mapToken
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 8, // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-                `<h3>${campground.title}</h3><p>${campground.location}</p>`
-            )
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
     )
-    .addTo(map)
+  )
+  .addTo(map);
