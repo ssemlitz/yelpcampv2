edb24cf86e4b0dcc75312e942678011f7af09d60
ssemlitz <sdsemlitz@yahoo.com>
2023-02-01 15:45:08 -0500

    implement helmet and use crossorigin on images for index and show page

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if(process.env.NODE_ENV !== "production"){
   require('dotenv').config()
 }
 
-// require('dotenv').config();
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
@@ -14,41 +13,101 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls } = require("./utils/helmetConfig");
 
 const userRoutes = require("./routes/users")
-const campgroundRoutes = require("./routes/campgrounds");
-const reviewRoutes = require("./routes/reviews");
+const campgroundRoutes = require("./routes/campgrounds")
+const reviewRoutes = require("./routes/reviews")
 
-mongoose.connect(process.env.DATABASE_URL);
+// Fix deprecated mongoose
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.DATABASE_URL)
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", () => {
-  console.log("Database Connected");
+  console.log("Database Connected")
 });
 
-const app = express();
+const app = express()
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(mongoSanitize({
+  replaceWith: '_'
+}))
 
 const sessionConfig = {
+  name: 'session',
   secret: "thisshouldbeabettersecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    //secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
-app.use(session(sessionConfig));
-app.use(flash());
+app.use(session(sessionConfig))
+app.use(flash())
+
+// app.use(helmet());
+// app.use(
+// 	helmet.contentSecurityPolicy({
+// 		directives: {
+// 			defaultSrc: [],
+// 			connectSrc: ["'self'", ...connectSrcUrls],
+// 			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+// 			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+// 			workerSrc: ["'self'", "blob:"],
+// 			objectSrc: [],
+// 			imgSrc: [
+// 				"'self'",
+// 				"blob:",
+// 				"data:",
+// 				`https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+// 				"https://images.unsplash.com/",
+// 			],
+// 			fontSrc: ["'self'", ...fontSrcUrls],
+// 		},
+// 	})
+// );
+
+app.use(
+  helmet({
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: {
+          allowOrigins: ['*']
+      },
+      contentSecurityPolicy: {
+          directives: {
+            defaultSrc: ['*'],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+              "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+          }
+      }
+  })
+)
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -59,21 +118,21 @@ passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
+  res.locals.success = req.flash("success")
+  res.locals.error = req.flash("error")
+  next()
 });
 
 app.use("/", userRoutes)
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/campgrounds", campgroundRoutes)
+app.use("/campgrounds/:id/reviews", reviewRoutes)
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home")
 });
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found!", 404));
+  next(new ExpressError("Page not found!", 404))
 });
 
 // 404 & other handling middleware at the end of the file if routes are not found
